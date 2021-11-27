40959c027f565470b718b3e30ae707e7c16cc42a
Casey Morford <casey@bigblueswimschool.com>
2021-11-27 09:05:17 -0600

    Update api

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,11 @@
-import { e911 } from "./inbound";
+import { e911, Searching } from "./inbound";
 
 export class Inbound {
   public e911: e911
+  public searching: Searching
 
   constructor (accountId: string, encodedToken: string) {
     this.e911 = new e911(accountId, encodedToken)
+    this.searching = new Searching(accountId, encodedToken)
   }
 }
